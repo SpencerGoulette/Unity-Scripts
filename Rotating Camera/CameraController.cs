3fed1e68aea1d8956502aef0850ce5318449e988
Spencer Goulette <32938977+SpencerGoulette@users.noreply.github.com>
2018-05-18 18:49:53 -0700

    Uploaded Scripts

---
+++ Rotating Camera/CameraController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+
+    public float yawSpeed = 2.0f;
+    public float pitchSpeed = 2.0f;
+    public float radiusSpeed = 5.0f;
+
+    private float radius = 10.0f;
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+    private float cameraX = 0.0f;
+    private float cameraY = 10.0f;
+    private float cameraZ = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    void FixedUpdate()
+    {
+        yaw += yawSpeed * Input.GetAxis("Mouse X");
+        pitch -= pitchSpeed * Input.GetAxis("Mouse Y");
+
+        radius += radiusSpeed * Input.GetAxis("Mouse ScrollWheel");
+        if(radius < 0)
+        {
+            radius = 0;
+        }
+        else if(radius > 25)
+        {
+            radius = 25;
+        }
+
+        cameraY = radius * Mathf.Sin(pitch / 57.3f);
+        cameraX = -radius * Mathf.Sin(yaw / 57.3f) * Mathf.Cos(pitch / 57.3f);
+        cameraZ = -radius * Mathf.Cos(yaw / 57.3f) * Mathf.Cos(pitch / 57.3f);
+        
+        // Dampen towards the target rotation
+        if(cameraY > 0)
+        {
+            transform.position = player.transform.position + new Vector3(cameraX, cameraY, cameraZ);
+        }
+        else
+        {
+            transform.position = player.transform.position + new Vector3(cameraX, 0, cameraZ);
+        }
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+    }
+}
---
+++ Rotating Camera/PlayerController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    // Player's body
+    private Rigidbody playerBody;
+    public GameObject cameraObject;
+    private int score = 0;
+    public Text scoreboard;
+    public Text win;
+    public float speed = 4.0f;
+
+    //Code that runs at the start of the game
+    private void Start()
+    {
+        playerBody = GetComponent<Rigidbody>();
+        score = 0;
+        SetScoreboard();
+        win.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        float horizontal = moveVertical * (transform.position.x - cameraObject.transform.position.x) + moveHorizontal * (transform.position.z - cameraObject.transform.position.z);
+        float vertical = moveVertical * (transform.position.z - cameraObject.transform.position.z) + -moveHorizontal * (transform.position.x - cameraObject.transform.position.x);
+
+        Vector3 movement = new Vector3(horizontal, 0.0f, vertical);
+
+        playerBody.AddForce(movement * speed);
+    }
+
+    //Trigger Event
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+            score++;
+            SetScoreboard();
+
+            //Checks to see if all 8 boxes have been collected
+            if(score >= 8)
+            {
+                win.text = "You Win!";
+            }
+        }
+    }
+
+    void SetScoreboard()
+    {
+        scoreboard.text = "Score: " + score.ToString();
+    }
+}
