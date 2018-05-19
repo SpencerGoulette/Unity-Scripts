b2f73a82a3543f445565c5a262ac19cd18b646cc
Spencer Goulette <32938977+SpencerGoulette@users.noreply.github.com>
2018-05-18 19:01:02 -0700

    Added comments to CameraController

--- Rotating Camera/CameraController.cs
+++ Rotating Camera/CameraController.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour {
 
+    // The player
     public GameObject player;
-
+    
+    // Speeds of Camera rotation and radius change
     public float yawSpeed = 2.0f;
     public float pitchSpeed = 2.0f;
     public float radiusSpeed = 5.0f;
-
+    
+    // Rotation, radius, and position of the camera
     private float radius = 10.0f;
     private float yaw = 0.0f;
     private float pitch = 0.0f;
@@ -17,22 +20,16 @@ public class CameraController : MonoBehaviour {
     private float cameraY = 10.0f;
     private float cameraZ = 0.0f;
 
-	// Use this for initialization
-	void Start () {
-        
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        
-	}
-
     void FixedUpdate()
     {
+	// Obtains rotation from mouse movement
         yaw += yawSpeed * Input.GetAxis("Mouse X");
         pitch -= pitchSpeed * Input.GetAxis("Mouse Y");
 
+	// Changes radius based off of mouse's scrollwheel
         radius += radiusSpeed * Input.GetAxis("Mouse ScrollWheel");
+	
+	// Sets limits for the radius
         if(radius < 0)
         {
             radius = 0;
@@ -42,11 +39,12 @@ public class CameraController : MonoBehaviour {
             radius = 25;
         }
 
+	// Calculates the position of the camera based off the radius, and two angles of rotation
         cameraY = radius * Mathf.Sin(pitch / 57.3f);
         cameraX = -radius * Mathf.Sin(yaw / 57.3f) * Mathf.Cos(pitch / 57.3f);
         cameraZ = -radius * Mathf.Cos(yaw / 57.3f) * Mathf.Cos(pitch / 57.3f);
         
-        // Dampen towards the target rotation
+        // Limits camera rotation to top hemisphere
         if(cameraY > 0)
         {
             transform.position = player.transform.position + new Vector3(cameraX, cameraY, cameraZ);
