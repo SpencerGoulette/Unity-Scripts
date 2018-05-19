ae437205c019d4181ff807a69d12b841cf3a1ae1
Spencer Goulette <32938977+SpencerGoulette@users.noreply.github.com>
2018-05-18 18:55:14 -0700

    Added comments on the PlayerController script

--- Rotating Camera/PlayerController.cs
+++ Rotating Camera/PlayerController.cs
@@ -7,60 +7,33 @@ public class PlayerController : MonoBehaviour {
 
     // Player's body
     private Rigidbody playerBody;
+    
+    // Camera
     public GameObject cameraObject;
-    private int score = 0;
-    public Text scoreboard;
-    public Text win;
+    
+    // Speed of player
     public float speed = 4.0f;
 
     //Code that runs at the start of the game
     private void Start()
     {
         playerBody = GetComponent<Rigidbody>();
-        score = 0;
-        SetScoreboard();
-        win.text = "";
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        // Obtains movement with mouse
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
-
+        
+        // Calculates how to move in the X and Z plane based off of the position of the player and the camera
         float horizontal = moveVertical * (transform.position.x - cameraObject.transform.position.x) + moveHorizontal * (transform.position.z - cameraObject.transform.position.z);
         float vertical = moveVertical * (transform.position.z - cameraObject.transform.position.z) + -moveHorizontal * (transform.position.x - cameraObject.transform.position.x);
-
+        
+        // Applies the calculated force onto the player
         Vector3 movement = new Vector3(horizontal, 0.0f, vertical);
 
         playerBody.AddForce(movement * speed);
-    }
-
-    //Trigger Event
-    void OnTriggerEnter(Collider other)
-    {
-        if(other.gameObject.CompareTag("Pickup"))
-        {
-            other.gameObject.SetActive(false);
-            score++;
-            SetScoreboard();
-
-            //Checks to see if all 8 boxes have been collected
-            if(score >= 8)
-            {
-                win.text = "You Win!";
-            }
-        }
-    }
-
-    void SetScoreboard()
-    {
-        scoreboard.text = "Score: " + score.ToString();
     }
 }
