07900e002899cbed58a16d08917e7c9e1224f3ff
Spencer Goulette <32938977+SpencerGoulette@users.noreply.github.com>
2018-06-17 10:51:12 -0700

    Uploaded MainMenu and TimeManager Script

---
+++ Platformer/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    // Play button is pressed
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Scenes/Testing");
+    }
+
+    // Quit button is pressed
+    public void QuitGame()
+    {
+        Debug.Log("Quit!");
+        Application.Quit();
+    }
+}
---
+++ Platformer/TimeManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class TimeManager : MonoBehaviour {
+
+    // Slow motion variables
+    public float slowMotionFactor = 0.05f;
+    public float slowMotionLength = 2.0f;
+
+    // Function to call for slow motion
+    public void SlowMotion ()
+    {
+        Time.timeScale = slowMotionFactor;
+        Time.fixedDeltaTime = Time.timeScale * .02f;
+    }
+
+    // Function to call to return to normal motion
+    public void NormalMotion ()
+    {
+        Time.timeScale += (1 / slowMotionLength) * Time.unscaledDeltaTime;
+        Time.timeScale = Mathf.Clamp(Time.timeScale, 0.0f, 1.0f);
+    }
+}
